51126989290c2067f674cde45d284603bb6fc2b7
Travis Hunter <travis.hunter@blueapron.com>
2016-11-04 13:34:34 -0400

    Make JSON conversion faster

--- src/saiyan/json_converter.cr
+++ src/saiyan/json_converter.cr
@@ -1,13 +1,29 @@
 module Saiyan
   alias AllParamTypes = Nil | String | Int64 | Float64 | Bool | Hash(String, JSON::Type) | Array(JSON::Type)
   alias JsonHash = Hash(String, AllParamTypes)
+  alias TerminalTypes = Nil | String | Int64 | Float64 | Bool | Array(JSON::Type) | JSON::Type
+
+  class JsonObject
+    @raw : Nil | String | Int64 | Float64 | Bool | Array(JSON::Type) | JSON::Type | Hash(String, TerminalTypes) | Hash(String, JsonObject) |  Array(Hash(String, JsonObject))
+
+    def initialize(@raw)
+    end
+
+    def to_s
+      @raw.to_s
+    end
+
+    def to_json(io)
+      @raw.to_json(io)
+    end
+  end
 
   class JsonConverter
     @json : JsonHash
-    @result : Hash(String, JSON::Any)
+    @result : Hash(String, JsonObject)
 
     def initialize(@json)
-      @result = Hash(String, JSON::Any).new
+      @result = Hash(String, JsonObject).new
     end
 
     getter json, result
@@ -20,7 +36,7 @@ module Saiyan
 
     private def convert(name, object)
       result.merge!(object_metadata(name + 's', object["id"]))
-      attributes = Hash(String, JSON::Any).new
+      attributes = Hash(String, JsonObject).new
 
       relationships = Hash(String, JSON::Type | Array(JSON::Type)).new
 
@@ -41,28 +57,28 @@ module Saiyan
 
     private def object_metadata(type_name, id)
       {
-        "id" => JSON::Any.new(id),
-        "type" => JSON::Any.new(type_name)
+        "id" => JsonObject.new(id),
+        "type" => JsonObject.new(type_name)
       }
     end
 
     private def insert_attribute(hash, key, value)
       return if ["id", "type"].includes?(key)
-      hash[key.as(String)] = JSON.parse(value.to_json)
+      hash[key.as(String)] = JsonObject.new(value)
     end
 
     private def process_relationship_metadata(relationships)
-      metadata = Hash(String, JSON::Any).new
+      metadata = Hash(String, JsonObject).new
 
-      relationships.as(Hash).each do |key, value|
+      relationships.each do |key, value|
         keystr = key.as(String)
-        attributes = Hash(String, JSON::Any).new
+        attributes = Hash(String, JsonObject).new
 
         if value.is_a?(Hash)
           object = object_metadata(keystr + 's', value.as(Hash)["id"])
           insert_attribute(attributes, "data", object)
         else
-          objects = [] of Hash(String, JSON::Any)
+          objects = [] of Hash(String, JsonObject)
           value.as(Array).each do |obj|
             objects << object_metadata(keystr, obj.as(Hash)["id"])
           end
@@ -76,11 +92,11 @@ module Saiyan
     end
 
     private def process_relationships(relationships)
-      data = [] of Hash(String, JSON::Any)
+      data = [] of Hash(String, JsonObject)
 
-      relationships.as(Hash).each do |key, value|
+      relationships.each do |key, value|
         keystr = key.as(String)
-        attributes = Hash(String, JSON::Any).new
+        attributes = Hash(String, JsonObject).new
 
         if value.is_a?(Hash)
           data << process(keystr + 's', value.as(Hash))
@@ -96,7 +112,7 @@ module Saiyan
 
     private def process(name, object)
       data = object_metadata(name, object["id"])
-      attributes = Hash(String, JSON::Any).new
+      attributes = Hash(String, JsonObject).new
 
       object.each do |key, value|
         insert_attribute(attributes, key, value)
